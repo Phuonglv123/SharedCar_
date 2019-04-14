17aafe82e8b52b259e5947977fc45a5817ec4e2d
phuonglv123 <vanphuong479@gmail.com>
2019-04-14 16:31:38 +0700

    fix back end

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use('/api/trip', require('./routes/api/api_Trips'));
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log('listening on port' + port);
 })
